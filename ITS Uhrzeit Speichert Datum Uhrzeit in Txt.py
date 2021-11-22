38e9635aacdaac9edaa52d276048b0c064fe867c
LeonFalk <93582320+LeonFalk@users.noreply.github.com>
2021-11-22 13:01:03 +0100

    Add files via upload

---
+++ ITS Uhrzeit Speichert Datum Uhrzeit in Txt.py
@@ -0,0 +1,48 @@
+import sys
+from collections import namedtuple
+from datetime import datetime as DateTime, timedelta as TimeDelta, date as Date
+from time import sleep
+
+HMS = namedtuple('HMS', 'hours minutes seconds')
+
+
+def time_delta2hms(time_delta):
+    minutes, seconds = divmod(time_delta.seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return HMS(hours % 60, minutes, seconds)
+
+
+class Clock(object):
+    def __init__(self, hours=0, minutes=0, seconds=0):
+        self.start = DateTime.now()
+        self.offset = TimeDelta(hours=hours, minutes=minutes, seconds=seconds)
+
+    def __str__(self):
+        return '%02d:%02d:%02d' % self.get_time()
+
+    def get_time(self):
+        return time_delta2hms(DateTime.now() - self.start + self.offset)
+
+    @classmethod
+    def from_system_time(cls):
+        now = DateTime.now()
+        return cls(now.hour, now.minute, now.second)
+
+
+def main():
+    clock = Clock.from_system_time()
+    
+    #Laufende Uhr auskommentiert
+    #while True:
+    #    sys.stdout.write('\r' + str(clock))
+    #    sys.stdout.flush()
+    #    sleep(0.5)
+
+    #Schreiben in ein Testdokument.txt -> Datum und Uhrzeit
+    datei = open('Testdokument.txt', 'a') 
+    datei.write(str(Date.today()))
+    datei.write('\t')
+    datei.write(str(clock))
+
+if __name__ == "__main__":
+    main()
---
+++ Speicherung in txt Dateien.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 16:05:39 2021
+
+@author: Leon
+Quelle: https://www.python-lernen.de/in-dateien-schreiben.htm
+"""
+
+###Textdatei öffnen und ausgeben
+###############################################################################
+#datei = open('Testdokument.txt', 'r') #-> 'r' für Read
+#print (datei.read())
+###############################################################################
+
+###Textdatei öffnen und anhängen
+###############################################################################
+#datei = open('Testdokument.txt', 'a') #-> 'a' für Anhängen
+#datei.write("\nZusatzhinweis")
+###############################################################################
+
+###Textdatei Überschreiben
+###############################################################################
+#datei = open('textdatei.txt','w')   #-> Überschreibt den Inhalt  
+#datei.write("\rZusatzhinweis")
+###############################################################################
+
+###Textdatei öffnen und schreiben -> aufpassen. Liest schneller, als das System schreibt
+###############################################################################
+#datei = open('Testdokument.txt', 'r+') #-> 'r+' für Read and Write
+#datei.write('neue Zeile')
+#print (datei.read())
+###############################################################################
+
+
+###Datumsintegration und Speicherung
+###############################################################################
+#import sys
+#from collections import namedtuple
+#from datetime import datetime as DateTime, timedelta as TimeDelta
+#from time import sleep
+
+
+#datei = open('Testdokument.txt', 'a') 
+#now = DateTime.now()
+#datei.write(now)
+###############################################################################
