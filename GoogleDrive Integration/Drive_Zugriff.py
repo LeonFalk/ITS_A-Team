c01a55a8f345bb24c9b13dfbe0eca30ebcb7e9cd
LeonFalk <93582320+LeonFalk@users.noreply.github.com>
2022-01-08 19:44:42 +0100

    Update Drive_Zugriff.py

--- GoogleDrive Integration/Drive_Zugriff.py
+++ GoogleDrive Integration/Drive_Zugriff.py
@@ -12,15 +12,23 @@ from oauth2client import file, client, tools
 from apiclient.http import MediaFileUpload
 
 ###     Erstellt Authentifizierung für die Google Drive API v3
-###     und gibt Drive_Service zurück -> Authentifizierungs-Token
-def create_authentication():
+###     benötigt:
+###         credentials.json (Authentifizierungsschlüssel)
+###         storage.json, wenn die Authentifizierung auf dem System bereits durchgeführt wurde...
+###     return:
+###         Drive_Service
+###         -> Authentifizierungs-Token für alle anderen Aufrufe nötig.
+###     Es fehlt noch:
+###     Falls Token nicht mehr aktuell/nicht erstellt ist läuft dies in einen Fehler
+###         -> dies muss noch abgefangen werden!!!
+def DRIVE_create_authentication():
         ###   Obtaining application credentials
-        ###   -> Zugriff auf die gesamte Drive-Struktur
+        ###   in diesem Fall Zugriff auf die gesamte Drive-Struktur
         SCOPES = (
             'https://www.googleapis.com/auth/drive',                  
             )
-        ###   Generiertes Token für die Authentifizierung speichern 
-        ###   -> keine neue Authentifizierung nötig
+        ###     Generiertes Token für die Authentifizierung speichern 
+        ###     wenn bereits geschehen ist keine neue Authentifizierung nötig
         store = file.Storage('storage.json')           
         creds = store.get()
         if not creds or creds.invalid:
@@ -30,49 +38,76 @@ def create_authentication():
         return DRIVE_Service
 
 ###     Erstellt einen Ordner im abgefragten Ordner 
-def create_folder(DRIVE_Service):
-    folder_id = input("Parent Folder ID? ....")
+###     Parameter:
+###         DRIVE_Service
+###         Ordnernamen (z.B. 'Records', Datum, Uhrzeit)
+def DRIVE_create_folder(DRIVE_Service, Ordnername):
+    
+    ###     Ordner wird im Hauptverzeichnis erstellt 'root'
+    folder_id = 'root'
     
     file_metadata = {
-        'name': 'Testordner',
+        'name': Ordnername,
         'parents': [folder_id],
         'mimeType': 'application/vnd.google-apps.folder'
     }
     file = DRIVE_Service.files().create(body=file_metadata,
                                     fields='id').execute()
-    print('Folder ID: %s' % file.get('id'))
-
-###     Lädt ein .txt Dokument aus dem Ordner /files hoch in den angefragten Ordner
-def create_upload(DRIVE_Service):
-    ### In welchen Ordner soll hochgeladen werden? -> sonst 'root'
-    folder_id = input("Parent Folder ID? ....")
     
+    ###     Für Testzwecke kann die Folder ID ausgegeben werden
+    #print('Folder ID: %s' % file.get('id'))
+    
+    ###     return der ID des neu erstellten Ordners
+    file_id = str(file.get('id'))
+    return file_id
+
+###     Lädt eine Datei in die Cloud hoch
+###     Parameter:
+###         folder_id       - Speicherort Cloud
+###         mimeType        - Dateityp  (String!: z.B. 'audio/wav', 'text/plain')
+###         file_name       - Dateiname (String!: z.B. 'Record_2022_01_08_....')
+###         file_location   - Dateiort  (String!: z.B. 'files/Testdokument.txt')
+def DRIVE_create_upload(DRIVE_Service, folder_id, mimetype, file_name, file_location):
     file_metadata = {
-                    'name': 'Testdokument.txt',
+                    'name': [file_name],
                     'parents': [folder_id]
     }
-    media = MediaFileUpload('files/Testdokument.txt',
-                            mimetype='text/plain',
+    media = MediaFileUpload(file_location,
+                            mimetype,
                             resumable=True)
     file = DRIVE_Service.files().create(body=file_metadata,
                                         media_body=media,
                                         fields='id').execute()
-    print ('File ID: %s' % file.get('id'))
+    
+    ###     return der ID der hochgeladenen Datei
+    file_id = str(file.get('id'))
+    return file_id
 
-###     Zeigt alle Dateien und Ordner an -> auch die im Papierkorb..
-def print_list_of_files(DRIVE_Service):        
+###     Zeigt alle Dateien und Ordner an. 
+###     Achtung: auch die im Papierkorb..
+def DRIVE_print_list_of_files(DRIVE_Service):        
         files = DRIVE_Service.files().list().execute().get('files', [])
         for f in files:
-            print(f['name'], f['mimeType'])
-            
+            print(f['name'], f['mimeType'], f['kind'])
+  
 ###     MAIN        
+###     - Für Testzwecke
 def main():
     ### Authentifizierung aufrufen und Drive_Service erstellen
-    DRIVE_Service = create_authentication()
+    DRIVE_Service = DRIVE_create_authentication()
     
-    #create_folder(DRIVE_Service)
-    #print_list_of_files(DRIVE_Service)
-    create_upload(DRIVE_Service)
+    ### Testordner erstellen
+    Ordnername = 'DiesIstNurEinTestordner'
+    folder_id = DRIVE_create_folder(DRIVE_Service, Ordnername)
     
-###     Funktionsaufruf
+    ### Testdokument in der erstellten Testordner hochladen
+    file_mimetype = 'text/plain'
+    file_name = 'Record_2022_01_08_....'
+    file_location = 'files/Testdokument.txt'
+    DRIVE_create_upload(DRIVE_Service, folder_id, file_mimetype, file_name, file_location)
+    
+    ### Alle Dateien/ Ordner in der Cloud ausgeben
+    #DRIVE_print_list_of_files(DRIVE_Service)
+    
+###     Funktionsaufruf Main()
 main()
